f55c755e2dd1299303570fd9cb27a9db95d3b00c
RubenGabrielian <gabrielianruben@gmail.com>
2020-04-22 21:54:41 +0400

    Add Banner

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,12 +6,13 @@ import { Title } from '../Styles/title'
 export const NavbarStyled = styled.div `
     background-color: ${pizzaRed};
     padding: 10px;
-    text-shadow: 8px -2px 4px #0000007d;
+    position: fixed;
+    width: 100%;
 `
 const Logo = styled(Title)`
     font-size: 20px;
     color: white;
-
+    text-shadow: 8px -2px 4px #0000007d;
 `
 
 export const Navbar = () => {
